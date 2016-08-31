05282b4be3c5233cbf2177544e4d9c0620e50097
xyyx <xyyx@mail.ru>
2016-09-01 07:23:08 +0800

    Add missed changelog.sh
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Exports
+dir=$ANDROID_BUILD_TOP
+out=$dir/out/target/product
+
 export Changelog=Changelog.txt
 
 if [ -f $Changelog ];
@@ -9,18 +13,19 @@ fi
 
 touch $Changelog
 
-echo "Generating changelog..."
+# Print something to build output
+echo ${bldppl}"Generating changelog..."${txtrst}
 
-for i in $(seq 14);
+for i in $(seq 10);
 do
 export After_Date=`date --date="$i days ago" +%m-%d-%Y`
 k=$(expr $i - 1)
 	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
 
 	# Line with after --- until was too long for a small ListView
-	echo '=======================' >> $Changelog;
+	echo '====================' >> $Changelog;
 	echo  "     "$Until_Date       >> $Changelog;
-	echo '=======================' >> $Changelog;
+	echo '===================='	>> $Changelog;
 	echo >> $Changelog;
 
 	# Cycle through every repo to find commits between 2 dates
@@ -30,6 +35,14 @@ done
 
 sed -i 's/project/   */g' $Changelog
 
+if [ -e $out/*/$Changelog ]
+then
+rm $out/*/$Changelog
+fi
+if [ -e $out/*/system/etc/$Changelog ]
+then
+rm $out/*/system/etc/$Changelog
+fi
 cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/
-rm -rf $Changelog
+rm $Changelog
