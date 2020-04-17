c4fe82f51abe166dcb3eda0073b69bad4362bedf
DennySPB <dennyspb@gmail.com>
2020-04-17 12:26:49 +0000

    Changelog: don't use repo for dir walking
    updated repo doesn't work with repo -p
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -21,11 +21,19 @@ export Until_Date=`date --date="$k days ago" +%F`
 echo "====================" >> $Changelog;
 echo "     $Until_Date    " >> $Changelog;
 echo "====================" >> $Changelog;
-repo forall -pc 'git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7' >> $Changelog
+while read path;
+do
+    # https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
+    Git_log=`git --git-dir ./${path}/.git log --after=$After_Date --until=$Until_Date --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=7`
+    if [ ! -z "${Git_log}" ]; then
+        printf "\n* ${path}\n${Git_log}\n" >> $Changelog;
+    fi
+done < ./.repo/project.list;
+
 echo "" >> $Changelog;
 done
 
-sed -i 's/project/ */g' $Changelog
+#sed -i 's/project/ */g' $Changelog
 sed -i 's/[/]$//' $Changelog
 
 if [ -e $out/*/$Changelog ]
