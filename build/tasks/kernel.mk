eee15130f2fb7efeb09f32391ff0eaaf2e6b2a40
baalajimaestro <me@baalajimaestro.me>
2020-06-18 21:29:57 +0530

    kernel: Add KERNEL_SUPPORTS_LLVM_TOOLS to permit building with LLVM Build Tools
    Selects LD, AR, NM, STRIP, OBJCOPY, OBJDUMP with their respective LLVM based binaries
    Needs a compatible toolchain, and a compatible kernel source.
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: If0227f0e0c03c91d1eddbba195ec1ced572d09a9
    Signed-off-by: Dakkshesh07 <dakkshesh5@gmail.com>
    Signed-off-by: Amsal Khan <md.amsalkhan@gmail.com>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -33,6 +33,7 @@
 #
 #   TARGET_KERNEL_CLANG_PATH           = Clang prebuilts path, optional
 #
+#
 #   KERNEL_SUPPORTS_LLVM_TOOLS         = If set, switches ar, nm, objcopy, objdump to llvm tools instead of using GNU Binutils, optional
 #
 #   BOARD_KERNEL_IMAGE_NAME            = Built image name
@@ -235,6 +236,23 @@ ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
     else
         # Use the default version of clang if TARGET_KERNEL_CLANG_VERSION hasn't been set by the device config
         KERNEL_CLANG_VERSION := $(LLVM_PREBUILTS_VERSION)
+    	endif
+
+    # As
+    ifeq ($(KERNEL_SUPPORTS_LLVM_TOOLS),true)
+        KERNEL_LD := LD=ld.lld
+        KERNEL_AR := AR=llvm-ar
+        KERNEL_OBJCOPY := OBJCOPY=llvm-objcopy
+        KERNEL_OBJDUMP := OBJDUMP=llvm-objdump
+        KERNEL_NM := NM=llvm-nm
+        KERNEL_STRIP := STRIP=llvm-strip
+    else
+        KERNEL_LD :=
+        KERNEL_AR :=
+        KERNEL_OBJCOPY :=
+        KERNEL_OBJDUMP :=
+        KERNEL_NM :=
+        KERNEL_STRIP :=
     endif
 
     # As 
