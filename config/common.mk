theOverpoweredGuy/android_vendor_zenx

vendor: Introduce UI Style Picker [2/3]
@ZeNiXxX:
* Allow changing UI shape
* Default, Round (M+L), Rectangle and NoCornerRadius

config/common.mk
@@ -153,6 +153,9 @@ include vendor/zenx/themes/Switch/switch.mk
 # Include ZenX Tiles Styles
 include vendor/zenx/themes/Tiles/tiles.mk
 
+# Include ZenX UI Styles
+include vendor/zenx/themes/UI/ui.mk
+
 # Overlays
 PRODUCT_PACKAGES += \
     NexusLauncherReleaseOverlay

CherishOS/android_vendor_cherish

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -97,6 +97,9 @@ include vendor/cherish/themes/BrightnessSlider/slider.mk
 # Include CherishOS Nav bar Styles
 include vendor/cherish/themes/Navbar/navbar.mk
 
+# Include CherishOS UI Styles
+include vendor/cherish/themes/UI/ui.mk
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images