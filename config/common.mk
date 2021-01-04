PixelExperience/vendor_aosp

vendor: Disable Google Play System Updates activity if apex disabled
Change-Id: If30c3b29e61026521078f0ed75ffb34ef8f8dcc7

config/common.mk
@@ -188,7 +188,16 @@ PRODUCT_PRODUCT_PROPERTIES += \
 #endif
 
 # Apex
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/aosp/config/apex.mk)
+else
+# Hide "Google Play System Updates" if Apex disabled
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/aosp/overlay_apex_disabled
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/aosp/overlay_apex_disabled/common
+endif
 
 # Audio
 $(call inherit-product, vendor/aosp/config/audio.mk)

CherishOS/android_vendor_cherish

Signed-off-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -149,7 +149,16 @@ include vendor/themes/themes.mk
 include vendor/cherish/config/ota.mk
 
 # Inherit from apex config
+ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/cherish/config/apex.mk)
+else
+# Hide "Google Play System Updates" if Apex disabled
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    vendor/cherish/overlay_apex_disabled
+
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/cherish/overlay_apex_disabled/common
+endif
 
 # Inherit from GMS product config
 ifeq ($(WITH_GAPPS),true)
@@ -183,7 +192,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ThemePicker \
 	 CherishThemesStub
-	
+	 
 #OmniJaws
 PRODUCT_PACKAGES += \
     OmniJaws \