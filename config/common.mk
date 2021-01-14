Evolution-X-AOSP/vendor_evolution

vendor: Only build Google Apex with Gapps

config/common.mk
@@ -119,6 +119,12 @@ include vendor/evolution/config/branding.mk
 # OTA
 include vendor/evolution/config/ota.mk
 
+# Inherit from CarrierSettings
+$(call inherit-product, vendor/evolution/config/common_telephony.mk)
+
+# Inherit from GMS product config
+ifeq ($(WITH_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
 # Inherit from apex config
 ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/evolution/config/apex.mk)
@@ -130,13 +136,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/evolution/overlay_apex_disabled/common
 endif
-
-# Inherit from CarrierSettings
-$(call inherit-product, vendor/evolution/config/common_telephony.mk)
-
-# Inherit from GMS product config
-ifeq ($(WITH_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_full.mk)
 endif
 
 # Pixel Style
@@ -152,7 +151,6 @@ PRODUCT_PACKAGES += \
     TouchGestures
 
 # Custom Overlays
-# Settings
 PRODUCT_PACKAGES += \
     SystemRavenBlackOverlay \
     SystemUIRavenBlackOverlay \

CherishOS/android_vendor_cherish

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -142,6 +142,12 @@ include vendor/themes/themes.mk
 # OTA
 include vendor/cherish/config/ota.mk
 
+# Inherit from CarrierSettings
+$(call inherit-product, vendor/cherish/config/common_telephony.mk)
+
+# Inherit from GMS product config
+ifeq ($(WITH_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_full.mk)
 # Inherit from apex config
 ifeq ($(TARGET_FLATTEN_APEX),false)
 $(call inherit-product, vendor/cherish/config/apex.mk)
@@ -153,13 +159,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/cherish/overlay_apex_disabled/common
 endif
-
-# Inherit from CarrierSettings
-$(call inherit-product, vendor/cherish/config/common_telephony.mk)
-
-# Inherit from GMS product config
-ifeq ($(WITH_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_full.mk)
 endif
 
 # Bootanimation