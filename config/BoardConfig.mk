PixelExperience/vendor_aosp

Define our avb key path if target supports custom avb key
Change-Id: I29e21e0f60e37ab98dc32b9afcd92616edb0adb2

config/BoardConfig.mk
@@ -4,6 +4,13 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/aosp/config/BoardConfigQcom.mk
 endif
 
+# Custom AVB Key
+ifneq ($(filter OFFICIAL CI,$(CUSTOM_BUILD_TYPE)),)
+ifeq ($(TARGET_USES_CUSTOM_AVB_KEY),true)
+include vendor/aosp/config/BoardConfigAvb.mk
+endif
+endif
+
 include vendor/aosp/config/BoardConfigSoong.mk
 
 # Namespace for fwk-detect

CherishOS/android_vendor_cherish

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/BoardConfig.mk
@@ -4,6 +4,12 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/cherish/config/BoardConfigQcom.mk
 endif
 
+# Custom AVB Key
+ifneq ($(filter OFFICIAL CI,$(CHERISH_BUILD_TYPE)),)
+ifeq ($(TARGET_USES_CUSTOM_AVB_KEY),true)
+include vendor/cherish/config/BoardConfigAvb.mk
+endif
+endif 
 include vendor/cherish/config/BoardConfigSoong.mk
 
 # Namespace for fwk-detect