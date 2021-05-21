9fa65acc5f38ac7124585fc25b35ddbe8ece3379
dusxtz <enyer.gustavoc@gmail.com>
2021-05-21 01:38:03 +0000

    update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 lunch_others_targets=()
-for device in $(python vendor/cherish/tools/get_official_devices.py)
+for device in $(python vendor/pubg/tools/get_official_devices.py)
 do
     for var in user userdebug eng; do
-        lunch_others_targets+=("aosp_$device-$var")
+        lunch_others_targets+=("pubg_$device-$var")
     done
 done
