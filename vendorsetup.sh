d332e96fc75803f4bfc84eec0b4887ea9f64ea88
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-06-19 10:01:34 +0700

    cherish:fix vendorsetup
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@ lunch_others_targets=()
 for device in $(python vendor/cherish/tools/get_official_devices.py)
 do
     for var in user userdebug eng; do
-        lunch_others_targets+=("aosp_$device-$var")
+        lunch_others_targets+=("cherish_$device-$var")
     done
 done
