10d9ecdc571e12fbc98f8e4806e3df94706b0954
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-08-12 13:37:36 +0700

    vendor:fix prebuilt app not build
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -38,3 +38,8 @@ LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
 LOCAL_PRODUCT_MODULE := true
 
 include $(BUILD_PREBUILT)
+
+# if some modules are built directly from this directory (not subdirectories),
+# their rules should be written here.
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
