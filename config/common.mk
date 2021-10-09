3729a37fbb4da82878ec803c4213162c3464c575
Dyneteve <dyneteve@hentaios.com>
2021-10-09 15:02:42 +0200

    vendor: overlay: Update from SP1A.210812.016
    Change-Id: I5ace644079507f9980619e7f502117e47d7dc329
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -277,7 +277,17 @@ $(call inherit-product, vendor/cherish/config/ota.mk)
 
 # GApps
 ifeq ($(WITH_GMS),true)
-$(call inherit-product, vendor/cherish/config/gapps.mk)
+$(call inherit-product, vendor/gms/gms_full.mk)
+
+# Common Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/cherish/overlay-gapps/common
+
+# Exclude RRO Enforcement
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+    vendor/cherish/overlay-gapps
+
+$(call inherit-product, vendor/cherish/config/rro_overlays.mk)
 else
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/cherish/overlay-aosp
 DEVICE_PACKAGE_OVERLAYS += vendor/cherish/overlay-aosp/common
