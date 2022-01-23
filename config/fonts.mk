a05708f77427090fd4c942db773930e35b09dd35
SuperDroidBond <superdroidbond@yahoo.com>
2022-01-23 23:19:11 +0900

    fonts: Import more font overlays
    * Imported from https://github.com/Project-Xtended/vendor_xtras/tree/xs/fonts
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Yash Pal <officialyash007@gmail.com>
    Co-authored-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: ashwatthama <sai404142@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -63,7 +63,36 @@ PRODUCT_PACKAGES += \
     FontLinotteSourceOverlay \
     FontManropeOverlay \
     FontOnePlusSansOverlay \
-    FontOneplusSlateSourceOverlay
+    FontOneplusSlateSourceOverlay \
+    FontAclonicaSourceOverlay \
+    FontAmaranteSourceOverlay \
+    FontAntipastoProSourceOverlay \
+    FontBariolSourceOverlay \
+    FontCagliostroSourceOverlay \
+    FontCoconSourceOverlay \
+    FontComfortaaSourceOverlay \
+    FontComicSansSourceOverlay \
+    FontCoolstorySourceOverlay \
+    FontEvolveSansSourceOverlay \
+    FontExotwoSourceOverlay \
+    FontFifa2018SourceOverlay \
+    FontFucekSourceOverlay \
+    FontGrandHotelSourceOverlay \
+    FontLGSmartGothicSourceOverlay \
+    FontLemonMilkSourceOverlay \
+    FontNokiaPureSourceOverlay \
+    FontNunitoSourceOverlay \
+    FontOdudaSourceOverlay \
+    FontQuandoSourceOverlay \
+    FontRedressedSourceOverlay \
+    FontReemKufiSourceOverlay \
+    FontRosemarySourceOverlay \
+    FontSamsungOneSourceOverlay \
+    FontSimpleDaySourceOverlay \
+    FontSonySketchSourceOverlay \
+    FontStoropiaSourceOverlay \
+    FontSurferSourceOverlay \
+    FontUbuntuSourceOverlay
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/cherish/fonts/prebuilt,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
