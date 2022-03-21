7434d3c89d2f52916a1c2f6f8817fc15a9ce62d0
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-03-21 12:39:51 +0700

    Enable OTA

--- config/ota.mk
+++ config/ota.mk
@@ -1,4 +1,9 @@
-ifneq ($(filter OFFICIAL CI,$(CUSTOM_BUILD_TYPE)),)
+ifeq ($(CHERISH_BUILD_TYPE), OFFICIAL)
+CHERISH_OTA_VERSION_CODE := twelve
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.cherish.ota.version_code=$(CHERISH_OTA_VERSION_CODE)
+    
 PRODUCT_PACKAGES += \
     Updates
 endif
