23b4fcc26d99d83b241e5d9335ff59176f3369f2
johnmart19 <johnivan19999@gmail.com>
2022-06-15 21:22:30 +0300

    vendor: Import Json generator from CrDroid
    Change-Id: Icf867e9492c82e9e1d7f562e7ae7b8838be50e5a
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -17,9 +17,19 @@
 CHERISH_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CHERISH_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(CHERISH_TARGET_PACKAGE)
+	$(hide) $(MD5) $(CHERISH_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CHERISH_TARGET_PACKAGE).sha256sum
 	$(hide) $(MD5) $(CHERISH_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(CHERISH_TARGET_PACKAGE).md5sum
-	@echo "Package Complete: $(CHERISH_TARGET_PACKAGE)" >&2
+	$(hide) ./vendor/cherish/build/tasks/createjson.sh $(TARGET_DEVICE) $(PRODUCT_OUT) $(CHERISH_VERSION).zip $(CHERISHVERSION)
+	@echo -e ${CL_CYN}""${CL_CYN}
+	@echo -e ${CL_CYN}"===========-Package Completed-==========="${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Zip: "${CL_YLW} $(CHERISH_TARGET_PACKAGE)${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"MD5: "${CL_YLW}" `cat $(CHERISH_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"SHA256: "${CL_YLW}" `sha256sum $(CHERISH_TARGET_PACKAGE) | cut -d ' ' -f 1` "${CL_RST}
+	@echo -e ${CL_BLD}${CL_YLW}"Size: "${CL_YLW}" `ls -l $(CHERISH_TARGET_PACKAGE) | cut -d ' ' -f 5` "${CL_RST}
+	@echo -e ${CL_CYN}"===========-----Thanks :)-----==========="${CL_RST}
+	@echo -e ""
