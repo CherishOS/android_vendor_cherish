21be223bd35630f374e72248d9c8d158db8501a0
Hưng Phan <phandinhhungvp2001@gmail.com>
2022-07-08 00:31:48 +0700

    Revert "build: Override host metadata"
    This reverts commit 896897fd123e01f674ac8ce4c41ad428727fd373.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -672,7 +672,3 @@ function fixup_common_out_dir() {
 }
 # Disable ABI checking
 export SKIP_ABI_CHECKS=true
-
-# Override host metadata to make builds more reproducible and avoid leaking info
-export BUILD_USERNAME=nobody
-export BUILD_HOSTNAME=android-build
