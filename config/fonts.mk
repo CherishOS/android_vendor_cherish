a683632f72779d8968dd760ce8996055dac6be83
minaripenguin <alexfinhart@gmail.com>
2022-07-17 19:58:32 +0800

    font: Add a Headline-only version of nothing font overlay
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -100,6 +100,7 @@ PRODUCT_PACKAGES += \
     FontUbuntuSourceOverlay \
 	 FontOppoSansOverlay \
     FontNothiingDotOverlay\
+    FontNothingDotHeadlineOverlay \
     FontProductSansVHOverlay 
 
 PRODUCT_COPY_FILES += \
