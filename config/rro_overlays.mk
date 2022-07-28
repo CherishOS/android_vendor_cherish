7013cff23eec4a8580aed766c32e209b65707129
Dyneteve <dyneteve@hentaios.com>
2022-07-28 22:26:35 +0200

    cherish: rro_overlays: Initial update for Tiramisu
    Change-Id: Iebae803260abdf9f7370c310933551e8d5095159
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -23,6 +23,7 @@ PRODUCT_PACKAGES += \
     PixelConfigOverlayCommon \
     TeleServiceOverlay \
     CaptivePortalLoginOverlay \
+    DMServiceOverlay \
     GoogleWebViewOverlay \
     TelecomOverlay \
     CellBroadcastServiceOverlay \
