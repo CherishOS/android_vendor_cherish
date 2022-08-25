PixelExperience/vendor_aosp

vendor: soong: Add TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB
Recently LOS changed KeyHandler to service. Service doesn't use system_server context, so FileObserver of old implementation can't access client info file. This implementation directly writes package name without using any files.

Signed-off-by: Shantanu Sarkar <shantanuplussarkar@gmail.com>
Change-Id: Ie69bac2adb8150f6954730907a9ec447877647cb

config/BoardConfigSoong.mk
@@ -28,6 +28,7 @@ SOONG_CONFIG_customGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     camera_needs_client_info \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -65,6 +66,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_customGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_customGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_customGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_customGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_customGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)

CherishOS/android_vendor_cherish

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/BoardConfigSoong.mk
@@ -35,6 +35,7 @@ SOONG_CONFIG_cherishGlobalVars += \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
     target_alternative_futex_waiters \
+    camera_needs_client_info_lib \
     target_init_vendor_lib \
     target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
@@ -67,6 +68,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_cherishGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_cherishGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_cherishGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_cherishGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_cherishGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)