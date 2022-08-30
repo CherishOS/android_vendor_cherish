f01a68261aa683d48a56584fe63a1bd48203d4e8
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:13:55 +0300

    config: Remove TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE
    This hasn't been ported after lineage-17.1
    
    Change-Id: I2aff9bb7bc17fa1444a904d47248d885268bf29b
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,7 +31,6 @@ SOONG_CONFIG_cherishGlobalVars += \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
-    ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
     needs_netd_direct_connect_rule \
     target_alternative_futex_waiters \
@@ -67,7 +66,6 @@ SOONG_CONFIG_cherishGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_N
 SOONG_CONFIG_cherishGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_cherishGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_cherishGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
-SOONG_CONFIG_cherishGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_cherishGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_cherishGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_cherishGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
