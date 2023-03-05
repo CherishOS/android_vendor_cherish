14fc3029a3f986a275c2621ccea2613ea318099e
Erfan Abdi <erfangplus@gmail.com>
2023-03-05 11:08:49 +0100

    vendor: Build desktop mode packages
    Change-Id: Iffa60be312957b9c6d733c24be0a7b6b9602ae77
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -209,6 +209,15 @@ endif
 PRODUCT_PACKAGES += \
     OmniJaws
 
+# DesktopMode
+PRODUCT_PACKAGES += \
+    DesktopMode
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+
+$(call inherit-product, packages/services/VncFlinger/product.mk)
+
 # # Hide nav Overlays
 PRODUCT_PACKAGES += \
     ImmersiveNavigationOverlay  
