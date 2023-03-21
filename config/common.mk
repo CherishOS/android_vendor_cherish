3169c475b4be3c2a1ba42cf0a80360ee1547bd72
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-03-22 00:03:21 +0700

    Revert "vendor: Adapt to new GMS repository"
    This reverts commit e2595f2ada3116ecf60d2cc967753d61959dff9d.

--- config/common.mk
+++ config/common.mk
@@ -302,8 +302,12 @@ include vendor/cherish/config/basicapps.mk
 
 else
 
-# GMS
-$(call inherit-product, vendor/gms/products/gms.mk)
+# Gapps
+ifeq ($(TARGET_USES_MINI_GAPPS),true)
+$(call inherit-product, vendor/gapps/config_mini.mk)
+else
+$(call inherit-product, vendor/gapps/config.mk)
+endif
 
 # RRO Overlays
 $(call inherit-product, vendor/cherish/config/rro_overlays.mk)
