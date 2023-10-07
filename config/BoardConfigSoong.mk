cfe5389f82078da4999b957c9f1d89e5f2d26862
maxwen <max.weninger@gmail.com>
2023-10-07 18:51:12 +0200

    soong: Follow the correct JSON syntax
    wrong: CROSS_COMPILE=\\"/data4...
    correct: CROSS_COMPILE=\"/data4...
    
    Change-Id: I5080aef7feb2ec7429cb9ba1f4baccefa61a3cd3
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -18,7 +18,7 @@ SOONG_CONFIG_cherishVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_cherishVarsPlugin += $(1)
-  SOONG_CONFIG_cherishVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_cherishVarsPlugin_$(1) := $($1)
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
