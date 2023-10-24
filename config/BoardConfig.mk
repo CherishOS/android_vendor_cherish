cdd4a9a80dd5e470bf8bc6941158fbd67c45fea3
Bruno Martins <bgcngm@gmail.com>
2023-10-25 00:53:43 +0100

    vendor: Move all QCOM board configs to hardware/qcom-caf/common
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,7 +1,7 @@
 include vendor/cherish/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/cherish/config/BoardConfigQcom.mk
+include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 # Custom AVB Key
