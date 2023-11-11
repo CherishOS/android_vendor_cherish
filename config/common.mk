a66a1780e2757707ffa892af67cccbbacd73fbdd
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-11-12 02:46:16 +0700

    vendor:Update GMS product config
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -314,8 +314,14 @@ include vendor/cherish/config/basicapps.mk
 
 else
 
-# GMS
-$(call inherit-product, vendor/gms/products/gms.mk)
+# Inherit from GMS product config
+ifeq ($(TARGET_USES_MINI_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_mini.mk)
+else ifeq ($(TARGET_USES_PICO_GAPPS),true)
+$(call inherit-product, vendor/gms/gms_pico.mk)
+else
+$(call inherit-product, vendor/gms/gms_full.mk)
+endif
 
 # RRO Overlays
 $(call inherit-product, vendor/cherish/config/rro_overlays.mk)
