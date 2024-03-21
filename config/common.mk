hentaiOS/platform_vendor_hentai

animations: Import pixel off-charging animation
Test: Verify charging animation.

Change-Id: I70dacfedbd6ab6234b9020c929cfe8d4c22fe661
Signed-off-by: Dyneteve <dyneteve@hentaios.com>

config/common.mk
@@ -70,6 +70,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
 # Inherit from lewdboratory config
 $(call inherit-product-if-exists, vendor/lewdboratory/packages.mk)
 
+# Inherit from animations config
+$(call inherit-product, vendor/hentai/config/animations.mk)
+
 # Inherit from apex config
 $(call inherit-product, vendor/hentai/config/apex.mk)
 

CherishOS/android_vendor_cherish

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -141,9 +141,8 @@ SYSTEM_OPTIMIZE_JAVA ?= true
 SYSTEMUI_OPTIMIZE_JAVA ?= true
 
 # Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
+# Inherit from animations config
+$(call inherit-product, vendor/cherish/config/animations.mk)
 
 # Filesystems tools
 PRODUCT_PACKAGES += \