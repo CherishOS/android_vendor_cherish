4d3727c5d1445d6fad5f0447603d5114df9fe127
Dhina17 <dhinalogu@gmail.com>
2024-05-26 15:27:50 +0530

    soong: Add libcameraservice extension config
    Change-Id: Id20f782c1008fed1f15250129618326091f61bf8
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -36,6 +36,9 @@ endif
 ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
     $(call soong_config_set,camera,package_name,$(TARGET_CAMERA_PACKAGE_NAME))
 endif
+ifneq ($(TARGET_CAMERA_SERVICE_EXT_LIB),)
+    $(call soong_config_set,libcameraservice,ext_lib,$(TARGET_CAMERA_SERVICE_EXT_LIB))
+endif
 ifneq ($(TARGET_USES_MIUI_CAMERA),)
     $(call soong_config_set,camera,uses_miui_camera,$(TARGET_USES_MIUI_CAMERA))
 endif
