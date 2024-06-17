829e750082dfc38b3e1b3873a1e9a7ace69de699
rmp22 <195054967+rmp22@users.noreply.github.com>
2024-06-17 19:55:36 +0900

    config: add blur support
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -175,12 +175,19 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # BootAnimation
 include vendor/cherish/config/bootanimation.mk
 
-# Blurs
- PRODUCT_SYSTEM_EXT_PROPERTIES += \
-     ro.sf.blurs_are_expensive=1 \
-     ro.surface_flinger.supports_background_blur=1 \
-     ro.launcher.blur.appLaunch=0 \
-     persist.sys.sf.disable_blurs=1
+# Blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
 	 
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
