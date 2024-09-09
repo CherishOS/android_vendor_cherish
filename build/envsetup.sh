8c56bfbdd9e8bea409bd6e49ece20b42295d8250
Michael Bestas <mkbestas@lineageos.org>
2024-09-09 08:34:41 +0300

    build: Import check_product from lineage-21.0 build/make
    Change-Id: I6cb2856bbc15bc25fc3ad6a4c1a6aa6aa56057f1

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -58,6 +58,30 @@ function mk_timer()
     return $ret
 }
 
+# check to see if the supplied product is one we can build
+function check_product()
+{
+    local T=$(gettop)
+    if [ ! "$T" ]; then
+        echo "Couldn't locate the top of the tree. Try setting TOP." >&2
+        return
+    fi
+    if (echo -n $1 | grep -q -e "^cherish_") ; then
+        CHERISH_BUILD=$(echo -n $1 | sed -e 's/^cherish_//g')
+    else
+        CHERISH_BUILD=
+    fi
+    export CHERISH_BUILD
+
+        TARGET_PRODUCT=$1 \
+        TARGET_RELEASE=$2 \
+        TARGET_BUILD_VARIANT= \
+        TARGET_BUILD_TYPE= \
+        TARGET_BUILD_APPS= \
+        get_build_var TARGET_DEVICE > /dev/null
+    # hide successful answers, but allow the errors to show
+}
+
 function brunch()
 {
     breakfast $*
