5066f028db917e8ccabd2a04a96a1f654946ca82
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-28 20:27:05 +0900

    config: properties: Add pihooks default values
    * A16 QPR2 Beta 3.3
    
    Spoof BuildFingerprint as Pixel Beta
    
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not "Valid"
    
    Co-authored-by: Joel Lucas <joelcba57@gmail.com>
    Co-authored-by: Joey <joey@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- config/evolution.mk
+++ config/evolution.mk
@@ -1,3 +1,20 @@
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=frankel_beta \
+    persist.sys.pihooks_DEVICE?=frankel \
+    persist.sys.pihooks_ID?=BP41.250916.015.A1 \
+    persist.sys.pihooks_RELEASE?=12 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-10-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=google/frankel_beta/frankel:16/BP41.250916.015.A1/14394230:user/release-keys \
+    PihooksGmsFp="google/frankel_beta/frankel:16/BP41.250916.015.A1/14394230:user/release-keys" \
+    PihooksGmsModel="Pixel 10"
+
 # Evolution X packages
 PRODUCT_PACKAGES += \
     EvoEgg \
