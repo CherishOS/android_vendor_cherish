000d7d600388a6c577791d55ce6eac1b45619fc4
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-26 21:37:22 +0800

    common: Disable art debug builds
    Change-Id: I44e16c12c70e30dea8e85a9751cc94e4ff329898
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -128,6 +128,9 @@ ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 include vendor/cherish/config/lineage_sdk_common.mk
 endif
 
+ART_BUILD_TARGET_NDEBUG := false
+ART_BUILD_TARGET_DEBUG := false
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
@@ -140,6 +143,9 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # but also allow explicit overriding for testing and development.
 SYSTEM_OPTIMIZE_JAVA ?= true
 SYSTEMUI_OPTIMIZE_JAVA ?= true
+
+# Disable dex2oat debug
+USE_DEX2OAT_DEBUG := false
 
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
