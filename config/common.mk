424ff8ffad117d72e597e43ca1cfdf2718fe9b71
Dmitrii <bankersenator@gmail.com>
2025-07-23 14:25:55 +0700

    pihooks: update to latest panther_beta
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -182,15 +182,15 @@ USE_DEX2OAT_DEBUG := false
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=cheetah_beta \
-    persist.sys.pihooks_DEVICE?=cheetah \
-    persist.sys.pihooks_ID?=BP31.250502.008 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-05-05 \
+    persist.sys.pihooks_PRODUCT?=panther_beta \
+    persist.sys.pihooks_DEVICE?=panther \
+    persist.sys.pihooks_ID?=BP31.250523.010 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-06-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/cheetah_beta/cheetah:16/BP31.250502.008/13497110:user/release-keys" \
-    PihooksGmsModel="Pixel 7 Pro"
+    PihooksGmsFp="google/panther_beta/panther:16/BP31.250523.010/13667654:user/release-keys" \
+    PihooksGmsModel="Pixel 7"
 
 ifeq ($(WITH_GMS),false)
 # Storage manager
