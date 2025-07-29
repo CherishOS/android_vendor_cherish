69364ea97e9075b30a3d68d157e0f7839037946b
Hưng Phan <phandinhhungvp2001@gmail.com>
2025-07-29 13:50:05 +0700

    pihooks: add spoof pihooks_RELEASE and pihooks_SDK_INT

--- config/common.mk
+++ config/common.mk
@@ -185,8 +185,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_PRODUCT?=panther_beta \
     persist.sys.pihooks_DEVICE?=panther \
     persist.sys.pihooks_ID?=BP31.250523.010 \
+    persist.sys.pihooks_RELEASE?=12 \
     persist.sys.pihooks_SECURITY_PATCH?=2025-06-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PihooksGmsFp="google/panther_beta/panther:16/BP31.250523.010/13667654:user/release-keys" \
