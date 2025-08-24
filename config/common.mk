763205231ef552a62fb49ba0f09e241554707ebd
Pranav Temkar <pranavtemkar@gmail.com>
2025-08-24 19:26:10 +0700

    vendor: Update pihooks to latest caiman_beta
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -182,17 +182,17 @@ USE_DEX2OAT_DEBUG := false
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=panther_beta \
-    persist.sys.pihooks_DEVICE?=panther \
-    persist.sys.pihooks_ID?=BP31.250523.010 \
+    persist.sys.pihooks_PRODUCT?=caiman_beta \
+    persist.sys.pihooks_DEVICE?=caiman \
+    persist.sys.pihooks_ID?=BP31.250610.004 \
     persist.sys.pihooks_RELEASE?=12 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-06-05 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-07-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/panther_beta/panther:16/BP31.250523.010/13667654:user/release-keys" \
-    PihooksGmsModel="Pixel 7"
+    PihooksGmsFp="google/caiman_beta/caiman:16/BP31.250610.004/13769805:user/release-keys" \
+    PihooksGmsModel="Pixel 9 Pro"
 
 ifeq ($(WITH_GMS),false)
 # Storage manager
