a49cc9f7427f171f5207efc443feb88a1615a17a
Nick Reuter <nreuter85@gmail.com>
2025-09-11 08:05:58 +0000

    vendor: cherish: build: Update rust toolchain dynamically
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -26,6 +26,9 @@ EOF
 CLANG_VERSION=$(build/soong/scripts/get_clang_version.py)
 export LLVM_AOSP_PREBUILTS_VERSION="${CLANG_VERSION}"
 
+RUST_VERSION=$(grep 'RustDefaultVersion =' build/soong/rust/config/global.go | awk '{print $3}' | awk -F '"' '{print $2}')
+export RUST_AOSP_PREBUILTS_VERSION="${RUST_VERSION}"
+
 function mk_timer()
 {
     local start_time=$(date +"%s")
