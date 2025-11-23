LMODroid/platform_vendor_lmodroid

Remove useless addon.d file
* Avoid detection as custom ROM

Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf

config/common.mk
@@ -27,11 +27,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lmodroid/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/lmodroid/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/lmodroid/prebuilt/common/bin/50-lmodroid.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lmodroid.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-lmodroid.sh
+    vendor/lmodroid/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \

CherishOS/android_vendor_cherish

Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

config/common.mk
@@ -57,11 +57,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/cherish/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/cherish/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/cherish/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-lineage.sh
+    vendor/cherish/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \